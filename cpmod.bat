aaef62579a02de96e066e93fe783e8596d367544
whiterasbk <whiterasbk@qq.com>
2021-05-03 19:46:29 +0800

    cfg gitignore & cpmod

--- cpmod.bat
+++ cpmod.bat
@@ -1,1 +1,2 @@
-copy .\build\reobfJar\output.jar F:\Minecraft\WorkSpace\[test]1.12.2\1.12.2-modtest\mods\
+rem copy .\build\reobfJar\output.jar F:\Minecraft\WorkSpace\[test]1.12.2\1.12.2-modtest\mods\
+copy .\build\reobfJar\output.jar D:\Games\mc-game-dirs\1.12.2-modtest\mods
