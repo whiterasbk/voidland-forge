a14f49168d2e56742f421a34e608fe965422ca15
whiterasbk <whiterasbk@qq.com>
2021-05-03 16:10:28 +0800

    fixed cpmod.bat

--- cpmod.bat
+++ cpmod.bat
@@ -1,1 +1,1 @@
-copy D:\Demos\idea-2019\mcmod\voidland\build\reobfJar\output.jar D:\Games\mc-game-dirs\1.12.2-modtest\mods\
+copy .\build\reobfJar\output.jar D:\Games\mc-game-dirs\1.12.2-modtest\mods\
