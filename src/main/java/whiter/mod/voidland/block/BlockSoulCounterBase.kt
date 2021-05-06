e463bccf8a5f48e6f38f4f3809f60235cebf87a1
G-Breeze <1113823696@qq.com>
2021-05-06 19:04:51 +0800

    Merge branch 'dev' of https://github.com/whiterasbk/voidland into dev
                              # Conflicts:
                              #	.gradle/4.9/fileHashes/fileHashes.bin
                              #	.gradle/4.9/fileHashes/fileHashes.lock
                              #	src/main/java/whiter/mod/voidland/register.kt
                              others:
                           1.更新修改了各个模型的细节
                           2.新增万象台
                           3.关于模型朝向问题-todo
                           4.关于gui布局问题-todo
                           5.关于物品功能问题-todo

---
+++ src/main/java/whiter/mod/voidland/block/BlockSoulCounterBase.kt
@@ -0,0 +1,20 @@
+package whiter.mod.voidland.block
+
+import net.minecraft.block.material.Material
+import net.minecraft.block.state.IBlockState
+import net.minecraft.util.BlockRenderLayer
+import whiter.mod.voidland.BlockBaseWithItem
+
+class BlockSoulCounterBase : BlockBaseWithItem(Material.ROCK, "soul_counter_base") {
+    override fun isFullCube(state: IBlockState): Boolean {
+        return false
+    }
+
+    override fun isOpaqueCube(state: IBlockState): Boolean {
+        return false
+    }
+
+    override fun getBlockLayer(): BlockRenderLayer {
+        return BlockRenderLayer.TRANSLUCENT
+    }
+}
---
+++ src/main/java/whiter/mod/voidland/block/BlockSoulCounterCenter.kt
@@ -0,0 +1,21 @@
+package whiter.mod.voidland.block
+
+import net.minecraft.block.material.Material
+import net.minecraft.block.state.IBlockState
+import net.minecraft.util.BlockRenderLayer
+import whiter.mod.voidland.BlockBaseWithItem
+
+
+class BlockSoulCounterCenter : BlockBaseWithItem(Material.ROCK, "soul_counter_center") {
+    override fun isFullCube(state: IBlockState): Boolean {
+        return false
+    }
+
+    override fun isOpaqueCube(state: IBlockState): Boolean {
+        return false
+    }
+
+    override fun getBlockLayer(): BlockRenderLayer {
+        return BlockRenderLayer.TRANSLUCENT
+    }
+}
