00f79a5a2167e0acb148546e458a83f51fea53bc
G-Breeze <1113823696@qq.com>
2021-05-08 20:02:19 +0800

    updated: 1.新增滞魂土 2.新增滞魂草方块 3.新增滞魂石 4.新增滞魂原石 5.新增滞魂原木

--- src/main/java/whiter/mod/voidland/block/base/BlockVoidLog.kt
+++ src/main/java/whiter/mod/voidland/block/base/BlockVoidLog.kt
@@ -3,5 +3,5 @@ package whiter.mod.voidland.block.base
 import net.minecraft.block.material.Material
 import whiter.mod.voidland.BlockBaseWithItem
 
-class BlockVoidGrass : BlockBaseWithItem(Material.GRASS, "void_grass") {
+class BlockVoidLog : BlockBaseWithItem(Material.WOOD, "void_log") {
 }
--- src/main/java/whiter/mod/voidland/block/base/BlockVoidStone.kt
+++ src/main/java/whiter/mod/voidland/block/base/BlockVoidStone.kt
@@ -3,5 +3,5 @@ package whiter.mod.voidland.block.base
 import net.minecraft.block.material.Material
 import whiter.mod.voidland.BlockBaseWithItem
 
-class BlockVoidGrass : BlockBaseWithItem(Material.GRASS, "void_grass") {
+class BlockVoidStone : BlockBaseWithItem(Material.ROCK, "void_stone") {
 }
