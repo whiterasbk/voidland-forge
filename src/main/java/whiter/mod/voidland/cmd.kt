c2eaa6a86b7283db8c9d9bd2dad8418c413a6433
whiterasbk <whiterasbk@qq.com>
2021-05-11 13:53:50 +0800

    added debug.kt deleted sout in cmd.kt

--- src/main/java/whiter/mod/voidland/cmd.kt
+++ src/main/java/whiter/mod/voidland/cmd.kt
@@ -3,26 +3,12 @@ package whiter.mod.voidland
 import net.minecraft.command.CommandBase
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent
 
-object debug {
-    var x = 128
-    var y = 192
-}
-
 object cmds {
     val map = mutableMapOf<String, CommandBase>()
-
     fun initCommands(event: FMLServerStartingEvent) {
-
-        println("这里是map")
         println(map)
         for (each in map) {
-            println("正在初始化命令...")
-            println(each)
-            //if
-            // (each as VlCommandBase) .register(event)
-
             println(each is VlCommandBase)
-
             if (each.value is VlCommandBase) {
                 (each.value as VlCommandBase).register(event)
             }
@@ -33,10 +19,8 @@ object cmds {
 abstract class VlCommandBase(cmdname: String) : CommandBase() {
 
     var command: String = cmdname
-
     init {
         cmds.map[cmdname] = this
-//        println("正在添加命令...${cmds.map}")
     }
 
     override fun getName(): String {
@@ -44,8 +28,7 @@ abstract class VlCommandBase(cmdname: String) : CommandBase() {
     }
 
     fun register(event: FMLServerStartingEvent) {
-//        println(this)
-//        println("这回输中文")
+        vlregister.logger.info("RegisterCommand: ${this.name}")
         event.registerServerCommand(this)
     }
 }
---
+++ src/main/java/whiter/mod/voidland/debug.kt
@@ -0,0 +1,6 @@
+package whiter.mod.voidland
+
+object debug {
+    var x = 128
+    var y = 192
+}
