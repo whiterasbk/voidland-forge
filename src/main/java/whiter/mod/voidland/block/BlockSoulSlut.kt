71b13d5a287b359549948e5bec0415a6e60b0a75
G-Breeze <1113823696@qq.com>
2021-05-14 13:33:46 +0800

    updated: 1.粒子效果(todo)soul_slut

--- src/main/java/whiter/mod/voidland/block/BlockSoulSlut.kt
+++ src/main/java/whiter/mod/voidland/block/BlockSoulSlut.kt
@@ -4,8 +4,24 @@ import net.minecraft.block.Block
 import net.minecraft.block.SoundType
 import net.minecraft.block.material.Material
 import net.minecraft.block.state.IBlockState
+import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.BlockRenderLayer
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.EnumHand
+import net.minecraft.util.EnumParticleTypes
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.text.ITextComponent
+import net.minecraft.world.World
+import net.minecraftforge.common.ForgeHooks
+import net.minecraftforge.fml.relauncher.Side
+import net.minecraftforge.fml.relauncher.SideOnly
+import net.minecraftforge.server.permission.context.BlockPosContext
+import whiter.mod.voidland.Voidland
 import whiter.mod.voidland.annotation.RegisterBlock
+import whiter.mod.voidland.blocks
+import whiter.mod.voidland.guids
+import whiter.mod.voidland.vl
+import java.util.*
 
 /*
 * 灵魂槽
@@ -29,5 +45,43 @@ class BlockSoulSlut : Block(Material.ROCK) {
 
     override fun getBlockLayer(): BlockRenderLayer {
         return BlockRenderLayer.TRANSLUCENT
+    }
+    fun getSlutBonus(world: World, pos: BlockPos): Float {
+        return (if (blocks.map["voidland:soul_slut"]?.equals(this)!!) 1 else 0).toFloat()
+    }
+    @SideOnly(Side.CLIENT)
+    override fun randomDisplayTick(stateIn: IBlockState, worldIn: World, pos: BlockPos, rand: Random) {
+        super.randomDisplayTick(stateIn, worldIn, pos, rand)
+
+        for (i in -2..2) {
+            var j = -2
+            while (j <= 2) {
+                if (i > -2 && i < 2 && j == -1) {
+                    j = 2
+                }
+
+                if (rand.nextInt(8) == 0) {
+                    for (k in 0..1) {
+                        val blockpos = pos.add(0, 1, 0)
+                        if (getSlutBonus(worldIn, blockpos) > 0.0f) {
+                            if (!worldIn.isAirBlock(pos.add(i / 2, 0, j / 2))) {
+                                break
+                                this.setLightLevel(0F)
+                            }
+                            pos.add(i,k,j)
+                            this.setLightLevel(0.5F)
+                            worldIn.spawnParticle(EnumParticleTypes.ENCHANTMENT_TABLE, pos.x.toDouble() + 0.5, pos.y.toDouble() + 2.0, pos.z.toDouble() + 0.5, (i.toFloat() + rand.nextFloat()).toDouble() - 0.5, (k.toFloat() - rand.nextFloat() - 1.0f).toDouble(), (j.toFloat() + rand.nextFloat()).toDouble() - 0.5, *IntArray(0))
+                        }
+                    }
+                }
+                ++j
+            }
+        }
+
+    }
+    override fun onBlockActivated(worldIn: World, pos: BlockPos, state: IBlockState, playerIn: EntityPlayer, hand: EnumHand, facing: EnumFacing, hitX: Float, hitY: Float, hitZ: Float): Boolean {
+        val blockpos = pos.add(0, 1, 0)
+        worldIn.spawnParticle(EnumParticleTypes.FLAME,pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble(),0.0,0.0,0.0)
+        return true
     }
 }
---
+++ src/main/java/whiter/mod/voidland/item/ItemSoulCrystal.kt
@@ -0,0 +1,8 @@
+package whiter.mod.voidland.item
+
+import net.minecraft.item.Item
+import whiter.mod.voidland.annotation.RegisterItem
+
+@RegisterItem
+class ItemSoulCrystal: Item() {
+}
